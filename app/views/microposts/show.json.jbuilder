503ae611a39dd7b3d84e57e52879b7298d0b8df6
Keiichiro Bando <keiichirobando21@gmail.com>
2014-03-24 07:11:25 +0000

    Initialize repository

---
+++ app/views/microposts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @micropost, :id, :teacher_name, :content, :user_id, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Senseiten::Application.config.secret_key_base = 'fa305c9a76237967cb1cb5e6a7166b1f4546df5952b1cf80cf76aa25eb3efb75f6aca0052dd3b467b14032d4c1917f2e3cce27343c2dce761498d88acfe9c2dd'
