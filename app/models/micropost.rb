e23b8c12aecf3eb4b32736e606dbb4de216b285d
Keiichiro Bando <keiichirobando21@gmail.com>
2015-07-04 00:25:34 +0000

    initial commit

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,10 @@
 class Micropost < ActiveRecord::Base
 	belongs_to :user
-	validates :content, :length => { :maximum => 140 }
+	validates :content, :length => { :maximum => 140, :message => '140文字以内で入力して下さい。' }
+	validates :rate, :numericality => { :less_than_or_equal_to => 5, :more_than_or_equal_to => 1, :message => '1~5までの数字で入力して下さい。' }
+  
+  def order(teacher_name)
+    Micropost.order("teacher_name")
+  end
+  
 end
